e2f8e211569e2ee9b5a821ca84e638761061aa9d
Gustavo Molina Soares <soaress.gustavo16@gmail.com>
2022-04-27 11:20:33 -0300

    Update gravacomp.h
    matriculas

--- gravacomp.h
+++ gravacomp.h
@@ -1,3 +1,8 @@
+/* Gustavo Molina Soares 2020209 */
+/* Lucca Vieira Rocha 123123213 */
+
+
+
 #include <stdio.h>
 int gravacomp (int nstructs, void* valores, char* descritor, FILE* arquivo);
 void mostracomp (FILE* arquivo);
